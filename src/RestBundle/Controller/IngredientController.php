893939d4e76525ae3d0e2da3743c7d1a2dc9f67f
Axel Blazart <a.blazart@gmail.com>
2017-09-13 09:41:26 +0200

    Support fosrestbundle

--- src/RestBundle/Controller/IngredientController.php
+++ src/RestBundle/Controller/IngredientController.php
@@ -8,55 +8,65 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 use IngredientBundle\Entity\Ingredient;
-/*use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\ViewHandler;
-use FOS\RestBundle\View\View;*/
+use FOS\RestBundle\View\View;
 
 class IngredientController extends Controller
 {
    //Only for testing purpose to comment out
    /**
-    * @Route("/ingredients", name="ingredients_list")
-    * @Method({"GET"})
+    * @Rest\View()
+    * @Rest\Get("/ingredients")
     */
     public function getIngredientsAction(Request $request) {
         $ingredients = $this->get('doctrine.orm.entity_manager')
         ->getRepository('IngredientBundle:Ingredient')
         ->findAll();
 
+        //TODO: automatise this part
         $formatted = array();
         foreach($ingredients as $ingredient) {
             $formatted[] = $ingredient->getData(); 
         }
+
+        $view = View::create($formatted);
         //Seems ok, but lot of space character in json response for numerical data
-        return new JsonResponse($formatted);
+        return $view;
     }
 
+
    /**
-    * @Route("/find-ingredients", name="find_ingredients")
-    * @Method({"GET"})
+    * @Rest\View()
+    * @Rest\Get("/ingredients/{search}")
     */
-    public function findIngredientAction(Request $request) {
+    //TODO: Cache & Asynchrone
+    public function findIngredientsAction(Request $request) {
         //check if query param is here
-        $search = "";
-        if(!isset($request->query->all()['s']) || ($request->query->all()['s'] == null)) {
-            return new JsonResponse(null);
+        $search = $request->get('search');
+        if($search === null) {
+            return new JsonResponse(['message' => 'Query needed', Response::HTTP_NOT_FOUND]);
         }
 
-        //Search 
-        $search = $request->query->all()['s'];
-        print_r($search);
         $ingredients = $this->get('doctrine.orm.entity_manager')
             ->getRepository('IngredientBundle:Ingredient')
             ->searchByString($search);
 
+        if(empty($ingredients))
+            return new JsonResponse(['message' => 'No result', Response::HTTP_NOT_FOUND]);
+        
         //formate data
         $formatted = array();
         foreach($ingredients as $ingredient) {
             $formatted[] = $ingredient->getData();
         }
-        return new JsonResponse($formatted);
+
+        $view = View::create($formatted);
+        //Seems ok, but lot of space character in json response for numerical data
+        return $view;
     }
 
 }
