913e4a6317fd9957ce3b3581218d9ce6b34698b8
Axel Blazart <a.blazart@gmail.com>
2017-09-13 12:33:46 +0200

    add react base configuration plus input

---
+++ web/assets/js/searchpage.js
@@ -0,0 +1,30 @@
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'hello config'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event) {
+        alert('test');
+        event.preventDefault();
+    }
+    render() { 
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            );
+    }
+}
+ReactDOM.render(
+    <SearchForm />,
+    document.getElementById('wrapper')
+);
